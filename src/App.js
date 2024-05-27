f5d6307f98c6d684a1b17ba894a369dea7970920
ikedoji45anne <148642109+ikedoji45anne@users.noreply.github.com>
2024-05-27 06:11:06 +0100

    i imported an existing file

--- src/App.js
+++ src/App.js
@@ -5,21 +5,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <h1>
+          Goodbye Everyone
+        
+        </h1>
+        
       </header>
     </div>
   );
 }
 
 export default App;
+import "./App.css";
+import WeatherSearch from "./WeatherSearch";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <WeatherSearch />
+    </div>
+  );
+}
